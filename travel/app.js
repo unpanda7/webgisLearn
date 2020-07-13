da157597e6bd8be7b9ec19d12b079685c256419c
unpanda7 <unpanda7@163.com>
2020-07-13 22:26:05 +0800

    添加travel及完成基本功能

---
+++ travel/app.js
@@ -0,0 +1,52 @@
+// 地图中心点，陕西
+var centerPoint = [35.63452, 109.132287];
+
+// 创建 map 实例
+var map = L.map('map', {
+  center: centerPoint,
+  zoom: 5,
+  minZoom: 1,
+  maxZoom: 16,
+  attributionControl: !1  
+})
+
+// 创建 图层
+
+var mapServerUrl = "http://map.geoq.cn/ArcGIS/rest/services/ChinaOnlineStreetPurplishBlue/MapServer/tile/{z}/{y}/{x}";
+
+// 添加图层
+L.tileLayer(mapServerUrl, {
+  opacity: 1,
+  zIndex: 0
+}).addTo(map)
+
+// 自定义
+var attr = L.control.attribution()
+attr.addAttribution("旅游地图相册Demo——那些我去过的城市");
+attr.addAttribution('<a href="http://giscafer.com" target="_blank">@giscafer</a>');
+attr.addTo(this.map);
+
+// 数据请求
+
+$.get('./data/data.json', (res) => {
+  console.log('re', res)
+  drawFootPoint(res.rows)
+})
+
+// 图标icon介绍两个网站可以下载:www.iconfont.cn 和 www.easyicon.net
+var footIcon = L.icon({
+  iconUrl: './foot.png',
+  iconSize: [28, 28],
+  iconAnchor: [10, 10]
+});
+
+function drawFootPoint(data) {
+  data.forEach(p => {
+    // maker [纬度,经度]
+    let point = [p.latitude - 0, p.longitude - 0]
+    L.marker(point, { icon: footIcon }).addTo(map)
+      .bindPopup(
+        `<h3>${p['city']}</h3>${p['date']}<br>${p['remark']}<br>`
+      )
+  })
+}
